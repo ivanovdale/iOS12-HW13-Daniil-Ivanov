6203b3bf2f17ec0decc0065bec97ac5679d88981
ivanovdale <dania-spb@mail.ru>
2024-01-30 13:41:23 +0300

    implement settings VC

--- iOS12-HW13-Daniil-Ivanov/Sources/Controller/SettingsViewController.swift
+++ iOS12-HW13-Daniil-Ivanov/Sources/Controller/SettingsViewController.swift
@@ -7,12 +7,65 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+final class SettingsViewController: UIViewController {
+    private let settingsFactory: SettingsFactory
+    private var settings: [[Setting]]?
+
+    // MARK: Init
+
+    init(settingsFactory: SettingsFactory) {
+        self.settingsFactory = settingsFactory
+        self.settings = nil
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
     }
 
+    override func loadView() {
+        setupView()
+    }
 
+    // MARK: Setup
+
+    private func setupView() {
+        let view = SettingsView()
+        view.setupDelegate(delegate: self)
+        self.view = view
+    }
+
+    // MARK: Data
+
+    private func loadData() {
+        settings = settingsFactory.createGroupedSettings()
+    }
+}
+
+// MARK: - Extensions
+
+extension SettingsViewController: UITableViewDelegate {
+    
+}
+
+extension SettingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        settings?[section].count ?? 0
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        settings?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>, for: <#T##IndexPath#>)
+    }
 }
 
