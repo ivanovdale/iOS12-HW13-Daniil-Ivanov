9ca425180a697cbe849f63b2d2dbf074dec39a42
ivanovdale <dania-spb@mail.ru>
2024-01-30 11:48:02 +0300

    change setting name to type, change setting type to kind

--- iOS12-HW13-Daniil-Ivanov/Sources/Model/Setting.swift
+++ iOS12-HW13-Daniil-Ivanov/Sources/Model/Setting.swift
@@ -8,12 +8,33 @@
 import Foundation
 
 struct Setting<T> {
-    let name: String
     let type: SettingType
+    let kind: SettingKind
     let value: T?
 }
 
-enum SettingType {
+enum SettingType: String {
+    case airplaneMode = "Авиарежим"
+    case wifi = "Wi-Fi"
+    case bluetooth = "Bluetooth"
+    case mobileService = "Сотовая связь"
+    case personalHotspot = "Режим модема"
+    case vpn = "VPN"
+    case notifications = "Уведомления"
+    case soundsAndHaptics = "Звуки, тактильные сигналы"
+    case focus = "Фокусирование"
+    case screenTime = "Экранное время"
+    case general = "Основные"
+    case controlCentre = "Пункт управления"
+    case displayAndBrightness = "Экран и яркость"
+    case homeScreenAndAppLibrary = "Экран \"домой\" и библиотека приложений"
+    case accessibility = "Универсальный доступ"
+    case wallpaper = "Обои"
+    case standBy = "Ожидание"
+    case siriAndSearch = "Siri и Поиск"
+}
+
+enum SettingKind {
     case switcher
     case switcherWithDetails
     case connectionInfo
