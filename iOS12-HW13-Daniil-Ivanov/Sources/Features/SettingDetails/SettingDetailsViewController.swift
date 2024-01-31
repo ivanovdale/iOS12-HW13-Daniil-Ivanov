ce4484f1c3a8e91e2ed338c090b2c1af738dc79b
ivanovdale <dania-spb@mail.ru>
2024-01-31 16:38:44 +0300

    add template for detail view

---
+++ iOS12-HW13-Daniil-Ivanov/Sources/Features/SettingDetails/SettingDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingDetailsViewController.swift
+//  iOS12-HW13-Daniil-Ivanov
+//
+//  Created by Daniil (work) on 31.01.2024.
+//
+
+import UIKit
+
+class SettingDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+    
+}
