ef51086374b0238b5e30bed21f421100362bd0cb
ivanovdale <dania-spb@mail.ru>
2024-01-31 16:37:27 +0300

    implement setting table view

--- iOS12-HW13-Daniil-Ivanov/Sources/Features/Settings/View/SettingsTableView.swift
+++ iOS12-HW13-Daniil-Ivanov/Sources/Features/Settings/View/SettingsTableView.swift
@@ -9,12 +9,21 @@ import UIKit
 
 final class SettingsTableView: UITableView {
 
+    // MARK: Init
+
     override init(frame: CGRect, style: UITableView.Style) {
-        super.init(frame: .zero, style: .grouped)
+        super.init(frame: .zero, style: .insetGrouped)
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    // MARK: Setup
+
+    private func setupView() {
+        register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifier)
+        separatorInset.left = SettingsTableViewCellConstants.separatorInsetLeft
+    }
 }
