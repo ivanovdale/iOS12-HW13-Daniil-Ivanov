138f914846ee225f7ad2e774b6e7c6ba87de2757
ivanovdale <dania-spb@mail.ru>
2024-02-09 19:35:21 +0300

    improve code style

--- iOS12-HW13-Daniil-Ivanov/Sources/Features/Settings/View/SettingsTableViewCell.swift
+++ iOS12-HW13-Daniil-Ivanov/Sources/Features/Settings/View/SettingsTableViewCell.swift
@@ -132,6 +132,30 @@ class SettingsTableViewCell: UITableViewCell {
             }
         }
     }
+// MARK: - Extension
+
+extension UIView{
+    func configure<T: UIView>(view: T, block: @escaping (T) -> ()) -> T {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        block(view)
+        return view
+    }
+
+    func setViewGradient(frame: CGRect, startPoint: CGPoint, endPoint: CGPoint, colors: [UIColor], location: [NSNumber]){
+
+        let gradient = CAGradientLayer()
+        gradient.frame = frame
+        gradient.colors = colors.map{
+            $0.cgColor
+        }
+        gradient.startPoint = startPoint
+        gradient.endPoint = endPoint
+        gradient.locations = location
+
+        self.layer.addSublayer(gradient)
+    }
+}
+
 // MARK: - Constants
 
 fileprivate enum Constants {
